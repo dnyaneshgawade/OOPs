032d521b18f30007e268871054071427780edb5c
dnyana <dnyaneshgawade903@gmail.com>
2021-07-30 09:09:15 +0530

    [Dnyanesh] add : creating a json file

---
+++ EnventoryManagement/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EnventoryManagement
+{
+    class Program
+    {
+        const string INVENTORY_JSON= @"C:\Users\dnyan\dnyana\EnventoryManagement\EnventoryManagementt\EnventoryManagement.json";
+        static void Main(string[] args)
+        {
+            EnventoryMain enventoryMain = new EnventoryMain();
+            enventoryMain.DisplayData(INVENTORY_JSON);
+        }
+    }
+}
